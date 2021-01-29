3dd653be0a66d698dd7e5bf1c7c40fa6cca9413e
433MHz <krystian.izdebski1999@gmail.com>
2021-01-29 19:11:41 +0100

    Repaired bug in shuffle mechanism

--- 001 puzzle game/script.js
+++ 001 puzzle game/script.js
@@ -2,27 +2,14 @@ var minusOne = [4,8,12,16,3,7,11,15,2,6,10,14];
 var plusOne = [1,5,9,13,2,6,10,14,3,7,11,15];
 var minusFour = [5,9,13,6,10,14,7,11,15,8,12,16];
 var plusFour = [1,5,9,2,6,10,3,7,11,4,8,12];
+var positions = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16];
 var divs;
-var positions = [];
 
 
-
-function shuffle(){
-    for(var i = 1; i<=16; i++){
-        var tempPos = Math.floor(Math.random() * 16) +1;
-
-        for(var x = 0; x < positions.length; x++){
-            for(var y = 0; y < positions.length; y++){     
-                while(positions[y] == tempPos){
-                    tempPos = Math.floor(Math.random() * 16) +1;   
-                }
-            }
-        }
-        positions[i-1] = tempPos; 
-    }
-    console.log(positions);
+function shuffle(array) {
+    array.sort(() => Math.random() - 0.5);
     putDivs();
-}
+  }
 
 function putDivs(){
     divs = [div1, div2, div3, div4, div5, div6, div7, div8, div9, div10, div11, div12, div13, div14, div15, div16];
@@ -34,17 +21,22 @@ function putDivs(){
     }
 
     for(var i = 0; i < divs.length; i++){
-        var htmlCode = "<div class=\"puzzle piece" + positions[i] + "\" onclick=\"slide(" +positions[i]+ ")\">";
+        var htmlCode
+        if(positions[i] == 16){
+            htmlCode = "<div class=\"puzzle piece" + positions[i] + "\"></div>";
+            divs[i].setAttribute('onclick', '');
+        }
+        else{
+            htmlCode = "<div class=\"puzzle piece" + positions[i] + "\">"+positions[i]+"</div>";
+        }
         divs[i].innerHTML = htmlCode;
     }
-
-    console.log(divs)
 }
 
-function slide(number){
-
+function slide(idNumber){
+    console.log(idNumber);
 }
 
 
 
-window.onload = shuffle;
+window.onload = shuffle(positions);
